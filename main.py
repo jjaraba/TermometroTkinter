ceeee08ce1c1eb2e4456f8ee0cde4ca56dbaaca5
Jose Luis Jaraba <jjaraba@hotmail.com>
2019-04-28 12:28:16 +0200

    Termometro con Tkinter

--- main.py
+++ main.py
@@ -2,16 +2,56 @@ from tkinter import *
 from tkinter import ttk
 
 class mainApp(Tk):
+    entrada = None
+    tipoUnidad = None
+    
+    __temperaturaAnt = ""
     
     def __init__(self):
         Tk.__init__(self)
         self.title("Termómetro")
-        self.geometry("200x150")
-        self.configure(bg = "#DED7B9")
+        self.geometry("150x200")
+        self.configure(bg = "#ECECEC")
+        self.resizable(0,0)
+        
+        self.temperatura = StringVar(value="")
+        self.temperatura.trace("w", self.validateTemperature)
+        self.tipoUnidad = StringVar(value="C")
+        
+        self.createLayout()
+        
+    def createLayout(self):
+        self.entrada = ttk.Entry(self, textvariable=self.temperatura).place(x=10, y=10)   
+
+        self.lblUnidad = ttk.Label(self, text="Grados:").place(x=10, y=45)
+        self.rb1 = ttk.Radiobutton(self, text="Fahrenheit", variable=self.tipoUnidad, value="F",command=self.selected).place(x=40, y=70)
+        self.rb2 = ttk.Radiobutton(self, text="Cesius", variable=self.tipoUnidad, value="C",command=self.selected).place(x=40, y=90)
         
     def start(self):
         self.mainloop()
-
+        
+    def validateTemperature(self, *args):
+        nuevoValor = self.temperatura.get()
+        try:
+            float(nuevoValor)
+            self.__temperaturaAnt = nuevoValor
+        except:
+            self.temperatura.set(self.__temperaturaAnt)
+            
+    def selected(self):
+        toUnidad = self.tipoUnidad.get()
+        grados = float(self.temperatura.get())
+        
+        if toUnidad == 'F':
+            resultado = grados * 9/5 +32
+        elif toUnidad == 'C':
+            resultado = (grados - 32) * 5/9
+        else:
+            resultado = grados
+            
+        self.temperatura.set(resultado)    
+        
+        
 
 if __name__ == '__main__':
     app = mainApp()
